6cd3c0c4a07b1e322fda038bfe2404ecbbe3069f
ATAKILIT-GIT <atakilt100@gmail.com>
2023-02-14 13:55:54 +0300

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,26 @@
+@@ -1,41 +1,39 @@
 #!/usr/bin/python3
+"""Log Parsing
+Write a script that reads stdin line by line and computes metrics:
 """
+import sys
+
 Log parsing
 """
 
+total_file_size = 0
+status = ['200', '301', '400', '401', '403', '404', '405', '500']
+obj = dict.fromkeys(status, 0)
 import sys
 
 if __name__ == '__main__':
 
+def printLogStat():
+    """Print log statistics"""
+    print("File size: {}".format(total_file_size))
+    for key, value in sorted(obj.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
     filesize, count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
@@ -17,11 +31,20 @@ if __name__ == '__main__':
             if v:
                 print("{}: {}".format(k, v))
 
+if __name__ == "__main__":
+    count = 0
     try:
         for line in sys.stdin:
+            line = line.split()
             count += 1
             data = line.split()
             try:
+                total_file_size += int(line[-1])
+
+                if line[-2] in status:
+                    obj[line[-2]] += 1
+
+            except (IndexError, ValueError):
                 status_code = data[-2]
                 if status_code in stats:
                     stats[status_code] += 1
@@ -31,9 +54,14 @@ if __name__ == '__main__':
                 filesize += int(data[-1])
             except BaseException:
                 pass
+
             if count % 10 == 0:
+                printLogStat()
                 print_stats(stats, filesize)
         print_stats(stats, filesize)
     except KeyboardInterrupt:
+        pass
+    finally:
+        printLogStat()
         print_stats(stats, filesize)
         raise
