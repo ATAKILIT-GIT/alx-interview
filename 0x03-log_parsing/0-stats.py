c3422550093dfc96dc22b1446954a6b195287bd8
ATAKILIT-GIT <atakilt100@gmail.com>
2023-02-17 12:30:36 +0300

    loghjj

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,42 @@
-#!/usr/bin/python3
-"""Log Parsing
-Write a script that reads stdin line by line and computes metrics:
-"""
-import sys
+import os
+
+import zero_log_parser as parser
 
 
-total_file_size = 0
-status = ['200', '301', '400', '401', '403', '404', '405', '500']
-obj = dict.fromkeys(status, 0)
+def parse_logs(dir_path, filename, output_suffix):
+    logfile_path = os.path.join(dir_path, filename)
+    new_output = os.path.splitext(logfile_path)[0] + output_suffix
+    logger = parser.console_logger(filename, verbose=True)
+    parser.parse_log(logfile_path, new_output, logger=logger)
+    return True
 
 
-def printLogStat():
-    """Print log statistics"""
-    print("File size: {}".format(total_file_size))
-    for key, value in sorted(obj.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+def main():
+    import sys
+    import argparse
+    arg_parser = argparse.ArgumentParser(
+        description='Run the log parser against a log file or directory of log files.')
+    arg_parser.add_argument('log_dir', help='directory of log files to parse into new output')
+    arg_parser.add_argument('--threads', type=int, default=1,
+                            help='number of threads to parse logs')
+    arg_parser.add_argument('--replace', action='store_true',
+                            help='whether to replace old outputs')
+    args = arg_parser.parse_args()
+    log_dir = args.log_dir
+    if not os.path.isdir(log_dir):
+        print("Not a directory: " + log_dir)
+        sys.exit(1)
+    replace = args.replace
+    output_suffix = '.txt' if replace else '.new.txt'
+    import multiprocessing as mp
+    pool = mp.Pool(processes=args.threads)
+    for dir_path, _, filenames in os.walk(log_dir):
+        log_names = list(filter(lambda x: parser.is_log_file_path(x), filenames))
+        log_args = [(dir_path, log_name, output_suffix,) for log_name in log_names]
+        pool.starmap_async(parse_logs, log_args)
+    pool.close()
+    pool.join()
 
 
-if __name__ == "__main__":
-    count = 0
-    try:
-        for line in sys.stdin:
-            line = line.split()
-            count += 1
-            try:
-                total_file_size += int(line[-1])
-
-                if line[-2] in status:
-                    obj[line[-2]] += 1
-
-            except (IndexError, ValueError):
-                pass
-
-            if count % 10 == 0:
-                printLogStat()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        printLogStat()
+if __name__ == '__main__':
+    main()
